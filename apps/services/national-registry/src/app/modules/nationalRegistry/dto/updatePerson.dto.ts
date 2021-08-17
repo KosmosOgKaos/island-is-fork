6bf24f6f7b1562b2089c37ed6c6ade09da4c6518
Magnus Skulason <maggisk@gmail.com>
2021-08-17 12:09:27 +0000

    fix updatePerson.dto

--- apps/services/national-registry/src/app/modules/nationalRegistry/dto/updatePerson.dto.ts
+++ apps/services/national-registry/src/app/modules/nationalRegistry/dto/updatePerson.dto.ts
@@ -4,24 +4,28 @@ import { ApiProperty } from '@nestjs/swagger'
 
 export class UpdatePersonDto {
   @ApiProperty()
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   fullName!: string
 
   @ApiProperty()
+  @IsOptional()
   @IsString()
   address!: string
 
   @ApiProperty()
+  @IsOptional()
   @IsEmail()
   email!: string
 
   @ApiProperty()
+  @IsOptional()
   @IsString()
   phone!: string
 
   @ApiProperty()
   @IsOptional()
   @IsNationalId()
-  partner_id!: string
+  partnerId!: string
 }
